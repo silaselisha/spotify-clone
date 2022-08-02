971efb5217cf6c2fb0b544e1d59f3682bb6c7a30
Elisha K. Silas <elisilas87@outlook.com>
2022-08-02 23:24:55 +0300

    Log user into spotify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Login from './components/Login'
-
-import React from 'react'
+import { getAccessToken } from './spotify'
 
 const App = () => {
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    const hash = getAccessToken()
+    window.location.hash = ''
+    const _token = hash.access_token
+
+    if(_token) {
+      setToken(_token)
+    }
+
+  }, [])
+
   return (
     <div className='app'>
-      <Login />
+      {
+        token ? (<h1>Logged into spotify</h1>) :
+        (<Login />)
+      }
     </div>
   )
 }
